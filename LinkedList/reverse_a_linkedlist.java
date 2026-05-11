a44ed0a83fc9779f3b1517b411870348ed3481cb
himani034 <160741763+himani034@users.noreply.github.com>
2026-05-11 22:26:28 +0530

    Add files via upload

---
+++ LinkedList/reverse_a_linkedlist.java
@@ -0,0 +1,16 @@
+// 206. Reverse Linked List
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+           ListNode  temp=curr.next;    
+           curr.next=prev;              
+           prev = curr;                
+           curr=temp;                   
+        }
+    return prev;
+    }
+}
